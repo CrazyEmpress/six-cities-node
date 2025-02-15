621af984987173a421a51e36abc93fd28feb2351
Kolyada Danil <kd_kein@mail.ru>
2025-02-15 19:44:09 +0300

    1.26. Приложение для терминала: Для менеджера команд описан метод processCommand исполняющий получаемые в качестве аргумента команды

--- src/cli/cli-application.ts
+++ src/cli/cli-application.ts
@@ -1,3 +1,4 @@
+import { CommandParser } from "./command-parser.js";
 import { Command } from "./commands/command.interface.js";
 
 type CommandCollection = Record<string, Command>;
@@ -6,6 +7,10 @@ export class CLIApplication {
 
   /** Коллекция всех команд имеющихся в приложении */
   private commands: CommandCollection = {};
+
+  constructor(
+    private readonly defaultCommand: string = '--help',
+  ) { }
 
   /**
    * "Регистрирует" переданные команды предварительно проверяя не были ли они уже зарегистрированы
@@ -19,5 +24,32 @@ export class CLIApplication {
 
       this.commands[command.getName()] = command;
     })
+  }
+
+  private getDefaltCommand(): Command | never {
+    if (!this.commands[this.defaultCommand]) {
+      throw new Error(`The default command ${this.defaultCommand} is not registered`)
+    }
+
+    return this.commands[this.defaultCommand];
+  }
+
+  private getCommand(commandName: string): Command {
+    return this.commands[commandName] ?? this.getDefaltCommand();
+  }
+
+  /**
+   * Исполняет полученную команду. В качестве аргумента получает массив строк
+   * который при помощи статического метода parse класса CommandParser
+   * будет преобразован в объект ParsedCommand
+   * из названия которого далее находится и исполняется зарегестрированная команда
+   * @param argv Массив строк, предполагаемая команда
+   */
+  public processComand(argv: string[]): void {
+    const parsedCommand = CommandParser.parse(argv);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.getCommand(commandName);
+    const commandArguments = parsedCommand[commandName] ?? [];
+    command.execute(...commandArguments)
   }
 }
