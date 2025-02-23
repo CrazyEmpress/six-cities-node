c04b3e3e8036434eadb93d34ad7e86ec30ab9014
Kolyada Danil <kd_kein@mail.ru>
2025-02-23 21:27:56 +0300

    2.12. Приложение для терминала. Часть 2: Описан тип MockServerData для данных получаемых с сервера

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,19 @@
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  publicationDates: string[];
+  cities: string[];
+  previewImages: string[];
+  images: string[];
+  isPremium: boolean[];
+  isFavorite: boolean[];
+  types: string[];
+  goods: string[];
+  hostNames: string[];
+  hostEmails: string[];
+  hostAvatars: string[];
+  hostPasswords: string[];
+  hostTypes: string[];
+  latitude: string[],
+  longitude: string[]
+};
