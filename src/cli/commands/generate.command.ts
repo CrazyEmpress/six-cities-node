f7623ccd2a06fe33e7862dd55aebe1ecc4e19d0c
Kolyada Danil <kd_kein@mail.ru>
2025-02-25 21:10:38 +0300

    2.12. Приложение для терминала. Часть 2: Дописана команда --generate - добавлен асинхронный метод write записывающий сгенерированные строки в файл

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,8 +1,12 @@
 import { Command } from './command.interface.js';
 
 import { MockServerData } from '../../shared/types/index.js';
+import { TSVOfferGenerator } from '../../shared/libs/offer-generator/tsv-offer-generator.js';
+
+import { appendFile } from 'node:fs/promises';
 
 import got from 'got';
+import chalk from 'chalk';
 
 export class GenerateCommand implements Command {
   private readonly RADIX = 10;
@@ -26,10 +30,24 @@ export class GenerateCommand implements Command {
 
     try {
       await this.load(serverUrl);
+      await this.write(filePath, count);
+      console.info(chalk.bgWhiteBright(`File ${filePath} was created!`));
     } catch (error: unknown) {
       if (error instanceof Error) {
         console.error(error.message);
       }
+    }
+  }
+
+  private async write(filePath: string, offerCount: number): Promise<void> {
+    const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+
+    for (let i = 0; i < offerCount; i++) {
+      await appendFile(
+        filePath,
+        `${tsvOfferGenerator.generate()}\n`,
+        { encoding: 'utf-8' }
+      );
     }
   }
 }
