1806709551df00f707939724f516ae73c88240af
Kolyada Danil <kd_kein@mail.ru>
2025-02-25 20:36:25 +0300

    2.12. Приложение для терминала. Часть 2: Добавлен запрос на получение данных для команды --generate. В файле tsconfig.json указано правило "strictPropertyInitialization": false, необходимое для того, чтобы объявление свойства класса без инициализации не вызывало ошибку линтера

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,14 +1,35 @@
 import { Command } from './command.interface.js';
 
+import { MockServerData } from '../../shared/types/index.js';
+
+import got from 'got';
+
 export class GenerateCommand implements Command {
   private readonly RADIX = 10;
+  private initialData: MockServerData;
 
   public getName(): string {
     return '--generate';
   }
 
-  public execute(...parameters: string[]): void {
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
+  public async execute(...parameters: string[]): Promise<void> {
     const [rowsCount, filePath, serverUrl] = parameters;
     const count = Number.parseInt(rowsCount, this.RADIX);
+
+    try {
+      await this.load(serverUrl);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 }
