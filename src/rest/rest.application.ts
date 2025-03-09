52350c7861011f334c8447c7ef4f2f3224399790
Kolyada Danil <kd_kein@mail.ru>
2025-03-09 18:24:59 +0300

    3.10. Архитектура приложения: Классы RestConfig и RestApplication переписаны для работы с пакетом convict, а также новым интерфейсом Config

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,10 +1,12 @@
 import { Config } from '../shared/libs/config/index.js';
+import { RestSchema } from '../shared/libs/config/rest.schema.js';
+
 import { Logger } from '../shared/libs/logger/index.js';
 
 export class RestApplication {
   constructor(
     private readonly logger: Logger,
-    private readonly config: Config
+    private readonly config: Config<RestSchema>
   ) { }
 
   public async init() {
