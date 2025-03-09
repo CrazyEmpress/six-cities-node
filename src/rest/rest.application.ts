d6598c4be47fade0c5d4e3c2a1da674c8d94740b
Kolyada Danil <kd_kein@mail.ru>
2025-03-09 20:09:17 +0300

    3.10. Архитектура приложения: Добавлен декоратор @injectable для класса RestApplication, добавлены декораторы @inject в качестве экземпляров классов реализующих интерфейсы Logger и Config

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,12 +1,16 @@
+import { inject, injectable } from 'inversify';
+
+import { Component } from '../shared/types/index.js';
+
 import { Config } from '../shared/libs/config/index.js';
 import { RestSchema } from '../shared/libs/config/rest.schema.js';
-
 import { Logger } from '../shared/libs/logger/index.js';
 
+@injectable()
 export class RestApplication {
   constructor(
-    private readonly logger: Logger,
-    private readonly config: Config<RestSchema>
+    @inject(Component.Logger) private readonly logger: Logger,
+    @inject(Component.Config) private readonly config: Config<RestSchema>
   ) { }
 
   public async init() {
