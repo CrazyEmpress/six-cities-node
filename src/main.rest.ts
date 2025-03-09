2f39ddf150ad836168a35d081d784856c732dbd2
Kolyada Danil <kd_kein@mail.ru>
2025-03-09 17:41:33 +0300

    3.10. Архитектура приложения: RestConfig добавлен в RestApplication. Класс RestApplication дописан так, чтобы принимать вторым аргументом экземпляр класса RestConfig

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,9 +1,12 @@
 import { RestApplication } from './rest/rest.application.js';
+import { RestConfig } from './shared/libs/config/rest.config.js';
 import { PinoLogger } from './shared/libs/logger/index.js';
 
 async function boostrap() {
   const logger = new PinoLogger();
-  const application = new RestApplication(logger);
+  const config = new RestConfig(logger);
+
+  const application = new RestApplication(logger, config);
 
   await application.init();
 }
