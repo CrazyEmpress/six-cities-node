e916e520c43393457c9fcebcdbd60bffefb500a6
Kolyada Danil <kd_kein@mail.ru>
2025-03-09 19:57:05 +0300

    3.10. Архитектура приложения: Добавлен контейнер с зависимостями. Теперь экземпляры классов будут получаться через него

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,14 +1,20 @@
-import { RestApplication } from './rest/rest.application.js';
-import { RestConfig } from './shared/libs/config/rest.config.js';
-import { PinoLogger } from './shared/libs/logger/index.js';
+import 'reflect-metadata';
+import { Container } from 'inversify';
 
-async function boostrap() {
-  const logger = new PinoLogger();
-  const config = new RestConfig(logger);
+import { Logger, PinoLogger } from './shared/libs/logger/index.js';
+import { RestApplication } from './rest/index.js';
+import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
+import { Component } from './shared/types/index.js';
 
-  const application = new RestApplication(logger, config);
 
+async function bootstrap() {
+  const container = new Container();
+  container.bind<RestApplication>(Component.RestApplication).to(RestApplication);
+  container.bind<Logger>(Component.Logger).to(PinoLogger);
+  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig);
+
+  const application = container.get<RestApplication>(Component.RestApplication);
   await application.init();
 }
 
-boostrap();
+bootstrap();
