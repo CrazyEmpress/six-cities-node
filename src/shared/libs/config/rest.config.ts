476222462d611ff6764ec56f9093fda095367b4c
Kolyada Danil <kd_kein@mail.ru>
2025-03-09 17:30:14 +0300

    3.10. Архитектура приложения: Класс RestConfig переписан для работы с переменными окружения через  пакет dotenv, а не напрямую

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,9 +1,24 @@
 import { Config } from './config.interface.js';
+import { Logger } from '../logger/index.js';
+
+import { DotenvParseOutput, config } from 'dotenv';
+
 
 export class RestConfig implements Config {
+  private readonly config: NodeJS.ProcessEnv;
+
   constructor(
-    private readonly config: NodeJS.ProcessEnv
-  ) { }
+    private readonly logger: Logger
+  ) {
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Can\'t read .env file. Perhaps the file doesn\'t exist');
+    }
+
+    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    this.logger.info('.env file was found and successfully parsed ');
+  }
 
   public get(key: string): string | undefined {
     return this.config[key];
